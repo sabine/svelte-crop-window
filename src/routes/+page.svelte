816ebf4b4721025bca95020480889a9f2a036be6
sabine <sabineschmaltz@gmail.com>
2022-10-07 16:08:56 +0200

    simplifications

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,12 @@
 	import Cropper from '$lib/cropper/Cropper.svelte';
 	import type { CropShape } from '$lib/cropper/geometry';
 
-	export let content_type: 'image' | 'video' = 'image';
-	export let url: string = '/succulents-3693409_1920.jpg';
+	let content_type: 'image' | 'video' = 'image';
+	let url: string = '/succulents-3693409_1920.jpg';
 
-	export let crop_shape: CropShape = 'rect';
+	let crop_shape: CropShape = 'rect';
 
-	export let crop = {
+	let crop = {
 		position: { x: 0, y: 0 },
 		aspect: 1.0,
 		rotation: 0,
@@ -20,17 +20,15 @@
 
 	<p>A component for cropping images and videos.</p>
 
-	{#if url}
-		<div style="height:100%;width:100%">
-			<Cropper
-				bind:value={crop}
-				media={{ url, content_type }}
-				aspect={crop.aspect}
-				{crop_shape}
-				on:change
-			/>
-		</div>
-	{/if}
+	<div style="height:100%;width:100%">
+		<Cropper
+			bind:value={crop}
+			media={{ url, content_type }}
+			aspect={crop.aspect}
+			{crop_shape}
+			on:change
+		/>
+	</div>
 
 	<p>Succulents on pixabay: <a href="https://pixabay.com/images/id-3693409/">https://pixabay.com/images/id-3693409/</a></p>
 </div>
