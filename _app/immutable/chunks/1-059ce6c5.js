703b9238499e780da16634ed3a8c7f0f80fc7b46
sabine <info@sabineschmaltz.de>
2022-10-15 19:40:36 +0200

    Updates

--- _app/immutable/chunks/1-059ce6c5.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-5756dcdf.js";export{t as component};
---
+++ _app/immutable/chunks/2-6bb1b90e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-1aba7e0c.js";export{t as component};
